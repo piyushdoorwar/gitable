05d153cf0cde65de453829ccae4690d8d620fe8e
Piyush Doorwar <piyushdoorwar@gmail.com>
2026-06-07 08:40:10 +0530

    style: improve sync badge layout and replace text arrows with SVG icons
    - Add flexbox layout to sync badge for proper alignment of text and icons
    - Replace text-based arrow characters (↑↓) with inline SVG icons for better visual consistency
    - Set line-height to 1 to prevent extra vertical spacing in badge

--- media/main.js
+++ media/main.js
@@ -1241,13 +1241,13 @@
       iconEl.innerHTML = ICONS.pull;
       labelEl.textContent = "Pull origin";
       btn.title = fetchedText;
-      badgeEl.textContent = `${s.behind}↓`;
+      badgeEl.innerHTML = `${s.behind}<svg viewBox="0 0 24 24" width="10" height="10" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C12.5523 3 13 3.44772 13 4V17.5858L18.2929 12.2929C18.6834 11.9024 19.3166 11.9024 19.7071 12.2929C20.0976 12.6834 20.0976 13.3166 19.7071 13.7071L12.7071 20.7071C12.3166 21.0976 11.6834 21.0976 11.2929 20.7071L4.29289 13.7071C3.90237 13.3166 3.90237 12.6834 4.29289 12.2929C4.68342 11.9024 5.31658 11.9024 5.70711 12.2929L11 17.5858V4C11 3.44772 11.4477 3 12 3Z" fill="currentColor"/></svg>`;
       badgeEl.classList.remove("hidden");
     } else if (s.ahead > 0) {
       iconEl.innerHTML = ICONS.push;
       labelEl.textContent = "Push origin";
       btn.title = fetchedText;
-      badgeEl.textContent = `${s.ahead}↑`;
+      badgeEl.innerHTML = `${s.ahead}<svg viewBox="0 0 24 24" width="10" height="10" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C12.2652 3 12.5196 3.10536 12.7071 3.29289L19.7071 10.2929C20.0976 10.6834 20.0976 11.3166 19.7071 11.7071C19.3166 12.0976 18.6834 12.0976 18.2929 11.7071L13 6.41421V20C13 20.5523 12.5523 21 12 21C11.4477 21 11 20.5523 11 20V6.41421L5.70711 11.7071C5.31658 12.0976 4.68342 12.0976 4.29289 11.7071C3.90237 11.3166 3.90237 10.6834 4.29289 10.2929L11.2929 3.29289C11.4804 3.10536 11.7348 3 12 3Z" fill="currentColor"/></svg>`;
       badgeEl.classList.remove("hidden");
     } else {
       iconEl.innerHTML = ICONS.refresh;
